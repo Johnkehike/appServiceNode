7acca459b7a746b311b4e2af16a81aa50bb36863
John Kehike <kehikejohnbull@yahoo.com>
2025-03-15 03:11:20 +0000

    test skipped

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-// app.js
+
 const express = require("express");
 const app = express();
-// const path = require("path");
+
 const usersRouter = require("./routes/usersRouter");
 
-// app.set("views", __dirname + "/src");
+
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/views" ));
 app.use(express.urlencoded({ extended: true }));
 app.use("/", usersRouter);
 
-// app.use(express.static(path.join(__dirname, "src")));
-// app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname, "src", "index.html"));
-// });
+
 
 
 const PORT = process.env.PORT || 3800;
